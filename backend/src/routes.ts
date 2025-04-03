40f3d05594e881f06d5bc8c852a527c8b32fe930
Erick-Albert1 <erick03plus@gmail.com>
2025-04-03 16:24:10 -0300

    fazendo a funcionalidade de salvar usuários no banco já criptografando senha

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,17 @@
 import { Router, Request, Response} from 'express'
 
+import { CreateUserController } from '././controllers/User/CreateUserController'
+
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response)=>{
+// router.get('/teste', (req: Request, res: Response)=>{
 
-    return res.json({ok: true})
+//     return res.json({ok: true})
 
-})
+// })
+
+
+//--- ROTAS USER ---
+router.post('/users', new CreateUserController().handle)
+
 export{router};
