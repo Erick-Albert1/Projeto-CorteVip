7fc1459ccf81bbec51f0191f3b91d6a9691b2892
Erick-Albert1 <erick03plus@gmail.com>
2025-04-03 21:13:30 -0300

    Finalizando funcionalidades de usuário, trazendo a opção do usuário atualizar dados como nome e endereço.

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response} from 'express'
 import { CreateUserController } from '././controllers/User/CreateUserController'
 import { AuthUserController } from './controllers/User/AuthUserController';
 import { DetailUserController } from './controllers/User/DetailUserController';
+import { UpdateUserController } from './controllers/User/UpdateUserController';
 
 import { isAthenticated } from './middlewares/isAuthenticated';
 
@@ -14,5 +15,6 @@ const router = Router();
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me',isAthenticated, new DetailUserController().handle)
+router.put('/userss',isAthenticated, new UpdateUserController().handle)
 
 export{router};
