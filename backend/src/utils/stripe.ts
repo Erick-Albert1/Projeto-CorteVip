bda34dbc4f519020911d7bc3b3c2734402534e8e
Erick-Albert1 <erick03plus@gmail.com>
2025-11-06 23:38:35 -0300

    componentes da stripe

--- backend/src/utils/stripe.ts
+++ backend/src/utils/stripe.ts
@@ -3,6 +3,7 @@ import Stripe from 'stripe';
 export const stripe = new Stripe(
     process.env.STRIPE_API_KEY,
     {
+        //componentes da stripe
         apiVersion: '2025-03-31.basil',
         appInfo:{
             name: 'CorteVip',
